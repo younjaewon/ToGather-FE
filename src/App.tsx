5231e53c6278f8c4bb4309e451d94cbe25d5b51e
Luke <101342819+onLuke@users.noreply.github.com>
2022-09-06 18:09:08 +0900

    style: src 내부 폴더 구조 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { lazy, Suspense, useEffect } from 'react';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route />
-        <Route />
-      </Routes>
-    </BrowserRouter>
-  );
+  // eslint-disable-next-line jsx-a11y/alt-text
+  return <img src="./assets/React-icon.svg" />;
 }
 
 export default App;
