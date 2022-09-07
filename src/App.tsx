808939c386629088413f4c076ba064c0343bf6cb
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-09-07 22:44:17 +0900

    Login Modal Css Layout 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { lazy, Suspense, useEffect } from 'react';
+import { lazy, Suspense, useEffect, useState } from 'react';
 import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
 import HeaderNavigation from './components/Header/HeaderNavigation';
+import ModalProvider from './contexts/ModalContext';
 
 const App = () => {
   return (
     <>
-      <HeaderNavigation />
+      <ModalProvider>
+        <HeaderNavigation />
+      </ModalProvider>
     </>
   );
 };
