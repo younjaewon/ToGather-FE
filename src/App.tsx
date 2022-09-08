dbb98536a61b9fd26096bf187b502b9b4f9c4523
Luke <101342819+onLuke@users.noreply.github.com>
2022-09-08 20:43:11 +0900

    feat: Navigation 메뉴 마크업 전반, 선택한 기술 스택 전역 관리 navigation hover시 각 메뉴에 따라 기술 스택 검색 영역, 좋아요 한 공고 목록, 기술 스택 통계 간략한 정보를 볼 수 있는 frame 작업 완료 기술 스택 이미지 등록 constants 폴더에 색상 코드 템플릿 일부 추가 selected된 기술 스택을 recoil 전역 state로 관리
    issue : recoil 전역 관리 데이터를 다른 파일로 분리해야 하는 지 여부

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-
 import React from 'react';
 import { lazy, Suspense, useEffect, useState } from 'react';
 import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
 import HeaderNavigation from './components/Header/HeaderNavigation';
 import ModalProvider from './contexts/ModalContext';
+import KakaoAuthPage from './pages/KakaoAuthPage';
+import Test from './pages/test';
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <ModalProvider>
         <HeaderNavigation />
+        <Routes>
+          <Route path="/" element={<Test />} />
+          <Route path="/oauth/kakao" element={<KakaoAuthPage />} />
+        </Routes>
       </ModalProvider>
-    </>
+    </BrowserRouter>
   );
 };
 
 export default App;
-
-
