e049bb5bb666de9179b7cdce7f2f99ef822f7993
Luke <101342819+onLuke@users.noreply.github.com>
2022-09-14 13:16:36 +0900

    Revert "fix: Nav 필터링 영역 오류 해결"
    This reverts commit f449d2193467c2ebf654223ab4b52f6ca8592646.

--- src/components/Header/searchMenu/SearchMenu.tsx
+++ src/components/Header/searchMenu/SearchMenu.tsx
@@ -31,7 +31,6 @@ const SearchMenu = ({ searchIsOpen }: SearchMenuProps) => {
       <TechsContainer>
         {techs.map(({ tech, icon }) => (
           <TechBtn 
-            className="tech__Btn"
             key={tech} 
             onClick={() => {
               handleTechBtn(tech);
