9600b8c99c448e3802b7dbb2114e386f2b18d935
Luke <101342819+onLuke@users.noreply.github.com>
2022-09-21 23:35:16 +0900

    refactoring_useInput refactoring (#38)
    * refactoring:리렌더링 감소를 위한 Ref 버젼 Hook 추가
    
    State로 관리되는 데이터를 Ref로 관리되게하는 버젼의 Hook추가
    연계된 form영역의 리렌더링이 필요없는 영역에 한해서 사용

--- src/hooks/useInputRef.ts
+++ src/hooks/useInputRef.ts
@@ -1,32 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
-const useInput = (initailValue: any) => {
-  const [form, setForm] = useState(initailValue);
+const useInputRef = (initailValue: any) => {
+  const formRef = useRef(initailValue);
 
-  const changeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const changeInput = (e: React.FocusEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setForm({ ...form, [name]: value });
+    formRef.current = { ...formRef.current, [name]: value };
   };
 
   const selectChange = (targetValue: any, targetName: any) => {
     const { name } = targetName;
     const { value } = targetValue;
-    setForm({ ...form, [name]: value });
+    formRef.current = { ...formRef.current, [name]: value };
   };
 
   const multiSelectChange = (targetValue: any, targetAction: any) => {
     const { action, name } = targetAction;
+
     if (action === 'clear') {
-      setForm({ ...form, [name]: [] });
+      formRef.current[name] = [];
     } else if (action === 'remove-value') {
-      const filterdData = form[name].filter(
+      const filterdData = formRef.current[name].filter(
         (item: number) => item != targetAction.removedValue.value
       );
-      setForm({ ...form, [name]: filterdData });
+      formRef.current[name] = filterdData;
     } else {
       for (let item of targetValue) {
         let { value } = item;
-        setForm({ ...form, [name]: [...form[name], value] });
+        formRef.current[name] = formRef.current[name] ? [...formRef.current[name], value] : [value];
       }
     }
   };
@@ -34,10 +35,10 @@ const useInput = (initailValue: any) => {
   const datePickerChange = (date: Date) => {
     const name = 'deadline';
     const dateValue = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-    setForm({ ...form, [name]: dateValue });
+    formRef.current[name] = dateValue;
   };
 
-  return { form, changeInput, selectChange, multiSelectChange, datePickerChange };
+  return { formRef, changeInput, selectChange, multiSelectChange, datePickerChange };
 };
 
-export default useInput;
+export default useInputRef;
