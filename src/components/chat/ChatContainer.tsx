86c2e7db97a37be6ad4d2c6c96e441eb485d6fb5
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-10-03 03:22:47 +0900

    chore: } 문법 오류  (#80)
    * chore: syntaxError '}' token 빠트림

--- src/components/chat/ChatContainer.tsx
+++ src/components/chat/ChatContainer.tsx
@@ -42,51 +42,52 @@ const ChatContainer = ({
   content,
   keyPress,
 }: IProps) => {
-const ChatContainer = ({ message, sendMessage, inputChange, content, keyPress }: IProps) => {
-  const bottomRef = useRef<HTMLDivElement>(null);
+  const ChatContainer = ({ message, sendMessage, inputChange, content, keyPress }: IProps) => {
+    const bottomRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (bottomRef.current) {
-      bottomRef.current.scrollTo({
-        top: bottomRef.current.scrollHeight,
-        behavior: 'smooth',
-      });
-    }
-  }, [message.length]);
+    useEffect(() => {
+      if (bottomRef.current) {
+        bottomRef.current.scrollTo({
+          top: bottomRef.current.scrollHeight,
+          behavior: 'smooth',
+        });
+      }
+    }, [message.length]);
 
-  return (
-    <MessageContainerBlock>
-      <MessagContainereWrapper>
-        <MessageBoxBlock>
-          <MessageRoomName>{roomName}</MessageRoomName>
-          <MessageBoxWrapper ref={bottomRef}>
-            {message.map(({ nickname, profileImage, message, time }, idx) => (
-              <MessageBox key={idx}>
-                <UserProfile>
-                  <img src={profileImage} alt="프로필" />
-                </UserProfile>
-                <div>
-                  <UserNickname>{nickname}</UserNickname>
-                  <BubbleMessage>{message}</BubbleMessage>
-                </div>
-                <MessageTime>{time}</MessageTime>
-              </MessageBox>
-            ))}
-          </MessageBoxWrapper>
-          <ChatMessageWrapper>
-            <ChatMessageInput
-              type="text"
-              value={content}
-              onChange={inputChange}
-              onKeyPress={keyPress}
-              placeholder="메시지를 입력해주세요."
-            />
-            <ChatSendButton onClick={sendMessage}>전송</ChatSendButton>
-          </ChatMessageWrapper>
-        </MessageBoxBlock>
-      </MessagContainereWrapper>
-    </MessageContainerBlock>
-  );
+    return (
+      <MessageContainerBlock>
+        <MessagContainereWrapper>
+          <MessageBoxBlock>
+            <MessageRoomName>{roomName}</MessageRoomName>
+            <MessageBoxWrapper ref={bottomRef}>
+              {message.map(({ nickname, profileImage, message, time }, idx) => (
+                <MessageBox key={idx}>
+                  <UserProfile>
+                    <img src={profileImage} alt="프로필" />
+                  </UserProfile>
+                  <div>
+                    <UserNickname>{nickname}</UserNickname>
+                    <BubbleMessage>{message}</BubbleMessage>
+                  </div>
+                  <MessageTime>{time}</MessageTime>
+                </MessageBox>
+              ))}
+            </MessageBoxWrapper>
+            <ChatMessageWrapper>
+              <ChatMessageInput
+                type="text"
+                value={content}
+                onChange={inputChange}
+                onKeyPress={keyPress}
+                placeholder="메시지를 입력해주세요."
+              />
+              <ChatSendButton onClick={sendMessage}>전송</ChatSendButton>
+            </ChatMessageWrapper>
+          </MessageBoxBlock>
+        </MessagContainereWrapper>
+      </MessageContainerBlock>
+    );
+  };
 };
 
 export default ChatContainer;
