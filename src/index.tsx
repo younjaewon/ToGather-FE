e231b357199be62b39aad6cf122543f3e28e9c25
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-10-10 16:22:52 +0900

    fix: firebase token 받아오기 (#107)

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,18 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
 
+// Firebase FCM ServiceWorker Setup
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('../firebase-messaging-sw.js')
+    .then(function (registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    })
+    .catch(function (err) {
+      console.log('Service worker registration failed, error:', err);
+    });
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
     <RecoilRoot>
